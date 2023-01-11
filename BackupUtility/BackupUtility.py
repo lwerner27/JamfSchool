308b6e8b31406dbd99d1bbc9357fca051ec29e93
Lucas Werner <lwerner27@gmail.com>
2023-01-10 18:01:35 -0600

    Started working on the backup utility

---
+++ BackupUtility/BackupUtility.py
@@ -0,0 +1,15 @@
+import base64
+import requests 
+import json
+
+network_id =  ""
+api_key = ""
+auth_value = base64.b64encode(bytes(network_id + ":" + api_key, "utf-8")).decode()
+
+headers = {"Authorization": "Basic " + auth_value}
+
+result = requests.get("https://lwernerjs.jamfcloud.com/api/devices", headers=headers)
+devices = result.json()["devices"]
+
+for device in devices:
+    print(json.dumps(device, indent=2))
