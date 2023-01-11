11b3bd9428cc32bec264ddba6a1b761dad74093f
Lucas Werner <lwerner27@gmail.com>
2023-01-10 21:55:47 -0600

    Updated the get request

--- BackupUtility/BackupUtility.py
+++ BackupUtility/BackupUtility.py
@@ -8,7 +8,7 @@ auth_value = base64.b64encode(bytes(network_id + ":" + api_key, "utf-8")).decode
 
 headers = {"Authorization": "Basic " + auth_value}
 
-result = requests.get("https://lwernerjs.jamfcloud.com/api/devices", headers=headers)
+result = requests.get("https://test.jamfcloud.com/api/devices", headers=headers)
 devices = result.json()["devices"]
 
 for device in devices:
